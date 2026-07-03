7b203992fd32a2115ef0856a71078d22dd069afe
Bhavesh Gunreddy <bhaveshreddygun@gmail.com>
2026-07-03 16:16:36 +0000

    Added battle menu n attack function.

--- text_rpg/rpg.py
+++ text_rpg/rpg.py
@@ -16,13 +16,57 @@ monster_hp = 60
 monster_max_hp = 60
 monster_attack = 12
 
-print("\n----- PLAYER -----")
-print(f"Name: {player_name}")
-print(f"HP: {player_hp}/{player_max_hp}")
-print(f"Mana: {player_mana}")
-print(f"Attack: {player_attack}")
+def display_stats():
+    print()
+    print("----- PLAYER -----")
+    print(f"Name: {player_name}")
+    print(f"HP: {player_hp}/{player_max_hp}")
+    print(f"Mana: {player_mana}")
+    print(f"Attack: {player_attack}")
+    print()
+    print("----- MONSTER -----")
+    print(f"Name: {monster_name}")
+    print(f"HP: {monster_hp}/{monster_max_hp}")
+    print(f"Attack: {monster_attack}")
+    
 
-print("\n----- MONSTER -----")
-print(f"Name: {monster_name}")
-print(f"HP: {monster_hp}/{monster_max_hp}")
-print(f"Attack: {monster_attack}")
+def attack():
+    global monster_hp 
+    #monster_hp = attack(monster_hp, player_attack)
+    monster_hp -= player_attack
+    
+    if monster_hp < 0:
+        monster_hp = 0
+        
+    print("You attacked the Goblin!")
+    
+    print(f"Goblin Hp: {monster_hp}/{monster_max_hp}")
+    
+display_stats()
+        
+        
+print()
+print("Choose your action:")
+print("1. Attack")
+print("2. Heal")
+print("3. Run")
+
+choice = input("Enter your choice (1-3):")
+
+if choice == "1":
+    print("You chose Attack!")
+    attack()
+    
+elif choice == "2":
+    print("You chose Heal!")
+    
+elif choice == "3":
+    print("You chose Run!")
+    
+else:
+    print("Invalid choice!")
+    
+
+        
+
+        
