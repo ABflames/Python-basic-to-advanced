5347857bdd1bd48b046ede1725e87d159d77365b
Bhavesh Gunreddy <bhaveshreddygun@gmail.com>
2026-07-07 16:11:15 +0000

    Add list and dictionary practice for RPG refactoring

---
+++ text_rpg/dictionary_practice.py
@@ -0,0 +1,27 @@
+student = {
+    "name" : "Bhavesh",
+    "age" : 20,
+    "branch" : "CSE"
+}
+
+#print(student)
+
+print(student["name"])
+
+student["age"] = 21
+
+student["University"] = "Mahindra University"
+
+print(student)
+
+monster = {
+    "name": "Goblin",
+    "hp": 60,
+    "attack": 12
+}
+
+print(monster["name"])
+
+#monster["hp"] -= player_attack
+monster["hp"] -= 20
+
---
+++ text_rpg/list_practice.py
@@ -0,0 +1,13 @@
+import random
+
+monster_names = [
+    "Goblin",
+    "Wolf",
+    "Zombie",
+    "Orc"
+]
+
+#print(monster_names)
+monster = random.choice(monster_names)
+#"Use the choice tool from the random toolbox."
+print(monster)
