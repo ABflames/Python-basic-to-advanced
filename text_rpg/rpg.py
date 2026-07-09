4c218f470bb31763e761f8f29652a8248ca0e76b
Bhavesh Gunreddy <bhaveshreddygun@gmail.com>
2026-07-09 09:31:11 +0000

    Complete Version 6 with multiple monsters

--- text_rpg/rpg.py
+++ text_rpg/rpg.py
@@ -1,3 +1,5 @@
+import random
+
 print("=" * 40)
 print("WELCOME TO THE RPG GAME !")
 print("=" * 40)
@@ -11,10 +13,44 @@ player_max_hp = 100
 player_mana = 30
 player_attack = 20
 
-monster_name = "Goblin"
-monster_hp = 60
-monster_max_hp = 60
-monster_attack = 12
+goblin = {
+    "name": "Goblin",
+    "hp": 60,
+    "max_hp": 60,
+    "attack": 12
+}
+
+wolf = {
+    "name": "Wolf",
+    "hp": 50,
+    "max_hp": 50,
+    "attack": 15
+}
+
+zombie = {
+    "name": "Zombie",
+    "hp": 80,
+    "max_hp": 80,
+    "attack": 8
+}
+
+orc = {
+    "name": "Orc",
+    "hp": 100,
+    "max_hp": 100,
+    "attack": 18
+}
+
+monsters = [
+    goblin,
+    wolf,
+    zombie,
+    orc
+]
+
+monster = random.choice(monsters)
+
+print(f"\nA wild {monster['name']} appeared!")
 
 def display_stats():
     print()
@@ -25,27 +61,27 @@ def display_stats():
     print(f"Attack: {player_attack}")
     print()
     print("----- MONSTER -----")
-    print(f"Name: {monster_name}")
-    print(f"HP: {monster_hp}/{monster_max_hp}")
-    print(f"Attack: {monster_attack}")
+    print(f"Name: {monster['name']}")
+    print(f"HP: {monster['hp']}/{monster['max_hp']}")
+    print(f"Attack: {monster['attack']}")
     
 # Functions must be like machines.
-def attack(monster_hp, player_attack):
+def attack(monster, player_attack): 
     #global monster_hp 
     
-    monster_hp -= player_attack
+    monster['hp'] -= player_attack
     
-    if monster_hp < 0:
-        monster_hp = 0
+    if monster['hp'] < 0:
+        monster['hp'] = 0
         
-    print("You attacked the Goblin!")
+    print(f"You attacked the {monster['name']}!")
     
-    print(f"Goblin HP: {monster_hp}/{monster_max_hp}")
+    print(f"{monster['name']} HP: {monster['hp']}/{monster['max_hp']}")
     
-    if monster_hp == 0:
-        print("You defeated the Goblin!")
+    if monster['hp'] == 0:
+        print(f"You defeated the {monster['name']}!")
         
-    return monster_hp
+    attack(monster, player_attack)
 
 def heal(player_hp, player_max_hp):
     #global player_hp
@@ -60,23 +96,23 @@ def heal(player_hp, player_max_hp):
     
     return player_hp
     
-def monster_turn(player_hp, monster_attack):
+def monster_turn(player_hp, monster):
     #global player_hp
     
-    player_hp -= monster_attack
+    player_hp -= monster['attack']
     
     if player_hp < 0:
         player_hp = 0
         
-    print("The Goblin attacked you!")
+    print(f"The {monster['name']} attacked you!")
     print(f"Player HP: {player_hp}/{player_max_hp}")
     
     if player_hp == 0:
-        print("You have been defeated by the Goblin!")
+        print(f"You have been defeated by the {monster['name']}!")
     
     return player_hp
 
-while player_hp > 0 and monster_hp > 0:   
+while player_hp > 0 and monster['hp'] > 0:   
         
         display_stats()
                 
@@ -89,33 +125,33 @@ while player_hp > 0 and monster_hp > 0:
         choice = input("Enter your choice (1-3):")
 
         if choice == "1":
-            monster_hp = attack(monster_hp, player_attack)
+            monster['hp'] = attack(monster, player_attack)
             
-            if monster_hp > 0:
-                player_hp = monster_turn(player_hp, monster_attack)
+            if monster['hp'] > 0:
+                player_hp = monster_turn(player_hp, monster)
             
             
         elif choice == "2":
             player_hp = heal(player_hp, player_max_hp)
             
-            if monster_hp > 0:
-                player_hp = monster_turn(player_hp, monster_attack)
+            if monster['hp'] > 0:
+                player_hp = monster_turn(player_hp, monster)
             
         elif choice == "3":
-            print("You chose Run!")
+            print("You ran away from battle!")
             break
             
         else:
             print("Invalid choice!")
         
-        #if monster_hp > 0:    
-        #    player_hp = monster_turn(player_hp, monster_attack)
+        #if monster['hp'] > 0:    
+        #    player_hp = monster_turn(player_hp, monster['attack'])
             
 if player_hp == 0:
     print("Game Over! You have been defeated.")
     
-elif monster_hp == 0:
-    print("Congratulations! You have defeated the Goblin!")
+elif monster['hp'] == 0:
+    print(f"Congratulations! You have defeated the {monster['name']}!")
     
 else:
     print("You ran away from the battle.")
